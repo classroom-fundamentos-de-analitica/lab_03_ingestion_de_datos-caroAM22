26243e2a2c95c048a399af65558b7f921b29e8e0
caroAM22 <caalvarezmu@unal.edu.co>
2024-03-06 10:31:35 -0500

    Update lab_03

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,50 @@ espacio entre palabra y palabra.
 
 
 """
+
 import pandas as pd
 
 
 def ingest_data():
+    df = pd.read_fwf(
+        "clusters_report.txt",
+        widths=[9, 16, 16, 80],
+        header=None,
+        names=[
+            "cluster",
+            "cantidad_de_palabras_clave",
+            "porcentaje_de_palabras_clave",
+            "-",
+        ],
+        skip_blank_lines=False,
+        converters={
+            "porcentaje_de_palabras_clave": lambda x: x.rstrip(" %").replace(",", ".")
+        },
+    ).drop([0, 1, 2, 3], axis=0)
 
-    #
-    # Inserte su código aquí
-    #
+    col4 = df["-"]
+    df = df[df["cluster"].notna()].drop("-", axis=1)
+    df = df.astype(
+        {
+            "cluster": int,
+            "cantidad_de_palabras_clave": int,
+            "porcentaje_de_palabras_clave": float,
+        }
+    )
 
+    column4 = []
+    text = ""
+    for lin in col4:
+        if isinstance(lin, str):
+            text += lin + " "
+        else:
+            text = ", ".join([" ".join(x.split()) for x in text.split(",")])
+            column4.append(text.rstrip("."))
+            text = ""
+            continue
+
+    df["principales_palabras_clave"] = column4
     return df
+
+
+print(ingest_data())
